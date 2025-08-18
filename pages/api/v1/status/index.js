e2a9e8a7f6da1c854432ced675f16ed5e976f39d
pedro-santos28 <phdss04@gmail.com>
2025-08-18 13:49:06 -0300

    add database module

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database";
+
+async function status(req, res) {
+  const result = await database.query("select 1+1 as sum");
+  console.log(result.rows[0]);
   res.status(200).json({
     status: "Ok",
     timestamp: new Date().toISOString(),
