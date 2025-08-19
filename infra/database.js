44620f86dde15c809b4256f36e2e0683a035efef
pedro-santos28 <phdss04@gmail.com>
2025-08-18 22:46:48 -0300

    adjust ssl injection into payload

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,29 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-  });
-
-  const ssl = process.env.NODE_ENV === "production" && {
-    rejectUnauthorized: true,
-    ca: Buffer.from(process.env.POSTGRES_SSL_CA, "base64").toString("utf-8"),
-  };
-
-  if (ssl) {
-    client.ssl = ssl;
+  const client = null;
+  if (process.env.NODE_ENV === "development") {
+    client = new Client({
+      user: process.env.POSTGRES_USER,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: process.env.POSTGRES_PORT,
+    });
+  } else {
+    client = new Client({
+      user: process.env.POSTGRES_USER,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: process.env.POSTGRES_PORT,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: Buffer.from(process.env.POSTGRES_SSL_CA, "base64").toString(
+          "utf-8",
+        ),
+      },
+    });
   }
 
   let result = null;
